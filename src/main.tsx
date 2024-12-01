fb68db05e8c5063fce8ee18bf6881a320110f93b
ironsummit72 <ironsumit72@gmail.com>
2024-12-01 16:00:36 +0530

    feat: integrate React Query and Axios in frontend
    - Added React Query for efficient data fetching and caching.
    - Integrated Axios for HTTP requests.
    - Updated project structure to include API service layer.
    - Implemented sample queries for testing data fetching.

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,11 @@
-
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import { RouterProvider } from 'react-router-dom'
-import router from '@/router/BrowserRouter'
-createRoot(document.getElementById('root')!).render(
-<RouterProvider router={router}/>
-)
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import { RouterProvider } from "react-router-dom";
+import router from "@/router/BrowserRouter";
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+const queryClient = new QueryClient();
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router} />
+  </QueryClientProvider>
+);
